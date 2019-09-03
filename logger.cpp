5e14c72543f331da19e13bd90582ce3144585b79
Armin Rothfuss <armin@rothfuss-web.de>
2019-09-03 23:02:53 +0200

    Fix wait_until_empty. Move logging::queue code to cpp

--- logger.cpp
+++ logger.cpp
@@ -99,6 +99,61 @@ namespace logging {
     return out;
   }
 
+  /// Enqueue an item and send signal to a waiting dequeuer.
+  void message_queue::enqueue (record&& t) {
+    {
+      std::lock_guard<std::mutex> lock(m_mutex);
+      m_queue.push(std::move(t));
+    }
+    m_condition.notify_all();
+  }
+
+  /// Dequeue an item if available, else waits until a new item is enqueued.
+  record message_queue::dequeue () {
+    std::unique_lock<std::mutex> lock(m_mutex);
+
+    wait_until_not_empty(lock);
+
+    if (m_queue.empty()) {
+      return record();
+    }
+
+    record item = m_queue.front();
+    m_queue.pop();
+    m_condition.notify_all();
+    return item;
+  }
+
+  /// Dequeue an item if available and return true, else return false.
+  bool message_queue::try_dequeue (record& t) {
+    std::unique_lock<std::mutex> lock(m_mutex);
+
+    if (m_queue.empty()) {
+      return false;
+    }
+
+    t = m_queue.front();
+    m_queue.pop();
+    m_condition.notify_all();
+    return true;
+  }
+
+  /// Waits until the queue is empty for maximum timeout time span.
+  void message_queue::wait_until_empty (const std::chrono::milliseconds& timeout) {
+    std::unique_lock<std::mutex> lock(m_mutex);
+
+    m_condition.wait_for(lock, timeout, [&]() {
+      return m_queue.empty();
+    });
+  }
+
+  /// Waits until the queue is no more empty.
+  void message_queue::wait_until_not_empty (std::unique_lock<std::mutex> &lock) {
+    m_condition.wait(lock, [&] () -> bool {
+                       return !m_queue.empty();
+                     });
+  }
+
   void core::logging_sink_call (core* core) {
     while (core->m_is_active) {
       record entry = core->m_messages.dequeue();
@@ -186,7 +241,7 @@ namespace logging {
       m_messages.enqueue(std::move(r));
       if (lvl >= level::error) {
         // wait up to 3 seconds until all messages are written!
-        m_messages.wait_until_empty(std::chrono::milliseconds(3000));
+        m_messages.wait_until_empty(std::chrono::milliseconds(1000));
       }
     } else {
       log_to_sinks(r);
