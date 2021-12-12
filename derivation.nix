c0c26729281567fb6a5621cb28999aae510fc0cd
Armin Rothfuß <armin@rothfuss-web.de>
2021-12-12 10:36:22 +0100

    Version 1.0.2. Add nix files. Change logging_version.h and _export.h location

---
+++ derivation.nix
@@ -0,0 +1,30 @@
+# https://discourse.nixos.org/t/how-to-use-a-nix-derivation-from-a-local-folder/5498/4
+
+{ stdenv, lib, pkgs } :
+
+stdenv.mkDerivation {
+  pname = "logging";
+  version = "1.0.2";
+
+  src = ./.;
+
+  nativeBuildInputs = with pkgs; [ cmake ];
+
+  enableParallelBuilding = true;
+
+  outputs = [ "dev" "out" ];
+
+  cmakeFlags = [
+    "-DLOGGING_CONFIG_INSTALL=ON"
+    "-DLOGGING_BUILD_STATIC_MODULE_LIB=OFF"
+  ];
+
+  meta = with lib; {
+    description = "A c++ logging library";
+    homepage = "https://github.com/r3dl3g/logging";
+    changelog = "https://github.com/r3dl3g/logging/releases/tag/v${version}";
+    license = licenses.mit;
+    maintainers = with maintainers; [ r3dl3g ];
+    platforms = with platforms; linux;
+  };
+}
