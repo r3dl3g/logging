eca3dd9bf68cc4f6085f2b3942c0b9670d9cbaa8
Armin Rothfuss <armin@rothfuss-web.de>
2025-08-05 09:49:27 +0200

    Change default Options Change build and install dir location

--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,4 @@
 echo ======= New Prepare at $(date) ======= >> prepare.log
-cmake -S. -Bbuild -DCMAKE_INSTALL_PREFIX=../libs -DCMAKE_PREFIX_PATH=../libs -DLOGGING_CONFIG_INSTALL=ON -DLOGGING_BUILD_STATIC_MODULE_LIB=OFF $* 2>&1 | tee -a prepare.log
+defines="-DCMAKE_INSTALL_PREFIX=$HOME/install -DCMAKE_PREFIX_PATH=$HOME/install"
+cmake -S. -B $HOME/build/${PWD##*/}/Debug -DCMAKE_BUILD_TYPE=Debug $defines $* 2>&1 | tee -a prepare.log
+cmake -S. -B $HOME/build/${PWD##*/}/Release -DCMAKE_BUILD_TYPE=Release $defines $* 2>&1 | tee -a prepare.log
