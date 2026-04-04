91d904c631ed83fc5a330613cdf8a735760cda31
Armin Rothfuss <armin@rothfuss-web.de>
2026-04-04 22:21:07 +0200

    Update project files, build with mingw

--- emc_prepare.sh
+++
@@ -1,9 +0,0 @@
-. ./.project_defines
-
-BUILD_PREFIX=~/.cache/build/emc
-INSTALL_PREFIX=~/.local/emc
-CMAKE_CMD="emcmake cmake"
-DEFINES="-DLOGGING_BUILD_DEPENDENT_LIBS=OFF -DLOGGING_NO_THREAD=ON -DVERBOSE=ON -DLOGGING_TESTS=ON"
-
-prepare Debug $@
-prepare Release $@
--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,13 @@
 . ./.project_defines
 
-DEFINES=-DLOGGING_TESTS=ON
+if [ "$PLATFORM" == "emc" ]; then
+    INSTALL_PREFIX=~/.local/emc
+    CMAKE_CMD="emcmake cmake"
+    DEFINES="-DLOGGING_BUILD_DEPENDENT_LIBS=OFF -DLOGGING_NO_THREAD=ON"
+elif [ "$PLATFORM" == "win32" ]; then
+    INSTALL_PREFIX=~/.local/win32
+    DEFINES="-DWIN32=ON -DLOGGING_BUILD_STATIC_MODULE_LIB=ON"
+fi
 
-prepare Debug
-prepare Release
+prepare Debug $@
+prepare Release $@
