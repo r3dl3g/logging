1eada0f3e52e3a3a51171d3a92337a6a1d6c1554
Armin Rothfuss <armin@rothfuss-web.de>
2026-04-12 13:14:23 +0200

    Update project files to use only cmake presets

--- prepare.sh
+++ prepare.sh
@@ -1,13 +1,4 @@
 . ./.project_defines
 
-if [ "$PLATFORM" == "emc" ]; then
-    INSTALL_PREFIX=~/.local/emc
-    CMAKE_CMD="emcmake cmake"
-    DEFINES="-DLOGGING_BUILD_DEPENDENT_LIBS=OFF -DLOGGING_NO_THREAD=ON"
-elif [ "$PLATFORM" == "win32" ]; then
-    INSTALL_PREFIX=~/.local/win32
-    DEFINES="-DWIN32=ON -DLOGGING_BUILD_STATIC_MODULE_LIB=ON"
-fi
-
-prepare Debug $@
-prepare Release $@
+prepare debug $@
+prepare release $@
